c2ff0c5218d95639a47b3bd91deedd31dbab0b56
Aytakin39 <aytakin.imanova@gmail.com>
2026-01-03 13:59:35 +0000

    7-print_chessboard.c

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,24 +1,19 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_array - prints n elements of an array of integers
- * @a: array of integers
- * @n: number of elements to print
- *
- * Description: Prints n elements separated by comma and space
+ * print_chessboard - Prints a chessboard
+ * @a: 2D array representing the chessboard
  */
-void print_array(int *a, int n)
+void print_chessboard(char (*a)[8])
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < 8; i++)
 	{
-		printf("%d", a[i]);
-
-		if (i != n - 1)
-			printf(", ");
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
 	}
-
-	printf("\n");
 }
