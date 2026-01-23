8a9bb9836b9d98dcd2d35d22d4fd3f2e49994641
Aytakin39 <aytakin.imanova@gmail.com>
2026-01-23 12:38:50 +0000

    main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,11 +1,11 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef main.h
+#define main.h
 
 /*
- * File: holberton.h
+ * File: main.h
  * Auth: Aytakin Imanova
  * Desc: Header file containing prototypes for all functions
- *       written in the 0x14-file_io directory.
+ *       written in the file_io directory.
  */
 
 #include <sys/types.h>
@@ -17,4 +17,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* HOLBERTON_H */
+#endif /* main.h */
